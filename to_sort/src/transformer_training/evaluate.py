7c1168cca4e08aa95ea10e996a249f9d37d4ec50
Amanda Bertsch <abertsch72@gmail.com>
2021-03-25 12:50:18 -0700

    error analysis

--- to_sort/src/transformer_training/evaluate.py
+++ to_sort/src/transformer_training/evaluate.py
@@ -6,10 +6,70 @@ import transformers
 from sklearn.metrics import precision_recall_fscore_support
 
 from src import transformer_training as models
-from src.transformer_training import get_all_data
+import tensorflow as tf
+import transformers
 
 
-MODEL_PATH = "saved_models/testmodel.11-1.96-f1at0.72727.hdf5"
+def from_transformer(transformer: transformers.TFPreTrainedModel,
+                     n_outputs: int) -> tf.keras.Model:
+
+    # Define inputs (token_ids, mask_ids, segment_ids)
+    token_inputs = tf.keras.Input(shape=(None,), name='word_inputs', dtype='int32')
+    mask_inputs = tf.keras.Input(shape=(None,), name='mask_inputs', dtype='int32')
+    segment_inputs = tf.keras.Input(shape=(None,), name='segment_inputs', dtype='int32')
+
+    # get contextualized token encodings from transformer
+    token_encodings = transformer([token_inputs, mask_inputs, segment_inputs])[0]
+
+    # get a sentence encoding from the token encodings
+    sentence_encoding = tf.keras.layers.GlobalMaxPooling1D()(token_encodings)
+
+    # Final output layer
+    outputs = tf.keras.layers.Dense(n_outputs, activation='sigmoid', name='outputs')(sentence_encoding)
+
+    # Define model
+    return tf.keras.Model(inputs=[token_inputs, mask_inputs, segment_inputs], outputs=[outputs])
+#from src.transformer_training import data.get_all_data
+
+import numpy as np
+import pandas as pd
+import transformers
+from typing import Mapping, Sequence, Text, Union
+import random
+def get_all_data(tokenizer: transformers.PreTrainedTokenizer, nonpeacock_filename, peacock_filename) -> (
+        np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+    df = [[line.strip(), 1] for line in open(peacock_filename).readlines()]
+    df.extend([[line.strip(), 0] for line in open(nonpeacock_filename).readlines()])
+    random.shuffle(df)
+    df = np.array(df)
+    x_raw = [data[0] for data in df]
+    x = from_tokenizer(tokenizer, [data[0] for data in df])
+    y = np.array([int(data[1]) for data in df])
+
+    return x_raw, x, y
+# converts texts into input matrices required by transformers
+def from_tokenizer(tokenizer: transformers.PreTrainedTokenizer,
+                   texts: Sequence[Text],
+                   pad_token: int = 0) -> Mapping[Text, np.ndarray]:
+    rows = [tokenizer.encode(text,
+                             add_special_tokens=True,
+                             max_length=tokenizer.model_max_length,
+                             truncation=True)
+            for text in texts]
+    shape = (len(rows), max(len(row) for row in rows))
+    token_ids = np.full(shape=shape, fill_value=pad_token)
+    is_token = np.zeros(shape=shape)
+    for i, row in enumerate(rows):
+        token_ids[i, :len(row)] = row
+        is_token[i, :len(row)] = 1
+    return dict(
+        word_inputs=token_ids,
+        mask_inputs=is_token,
+        segment_inputs=np.zeros(shape=shape))
+
+
+
+MODEL_PATH = "testmodel.03-0.23-f1at0.96296.hdf5"
 print(MODEL_PATH)
 def test_model(model_path: Text,
          positive_examples_file: Text,
@@ -33,7 +93,7 @@ def test_model(model_path: Text,
     test_data_rows = {positive_examples_file: [], negative_examples_file: []}
 
     # load the fine-tuned transformer model
-    model = models.from_transformer(transformer=transformer, n_outputs=1)
+    model = from_transformer(transformer=transformer, n_outputs=1)
     model.load_weights(model_path) #.expect_partial()
     raw_x, data_x, data_y = get_all_data(tokenizer, negative_examples_file, positive_examples_file)
 
@@ -64,7 +124,7 @@ def test_model(model_path: Text,
             print(row)
         print()
 
-nonpeacock = os.path.join("clean-nonpeacockterms.txt")
-peacock = os.path.join("clean-peacockterms.txt")
+nonpeacock = os.path.join("..", "..", "..", "data", "NEW-clean-nonpeacock-ir.txt")
+peacock = os.path.join("..", "..", "..", "data","NEW-clean-peacockterms.txt")
 
-test_model(MODEL_PATH, peacock, nonpeacock, "roberta-base", 6)
+test_model(MODEL_PATH, peacock, nonpeacock, "roberta-base", 8)
